5b917646f7f6efd51f9d64a5fdeb945161102645
Divij <divij292.be22@chitkara.edu.in>
2024-12-09 15:45:35 +0530

    Port number changed

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4001;
+const PORT = 4000;
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
